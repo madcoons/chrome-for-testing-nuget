7e68c0f4cb5a8cea41cd6335d0496dfe66c4fbbf
Miroljub <miroljub1995@gmail.com>
2024-05-20 15:43:31 +0200

    Update for windows

--- ChromeForTesting/ChromeForTesting.Tests/BasicTests.cs
+++ ChromeForTesting/ChromeForTesting.Tests/BasicTests.cs
@@ -8,10 +8,22 @@ public class BasicTests
     [Test]
     public async Task Test_Get_Version_Works_Async()
     {
-        BufferedCommandResult res = await Cli.Wrap(ChromeForTestingInstance.ChromePath)
-            .WithArguments("--version")
-            .WithValidation(CommandResultValidation.None)
-            .ExecuteBufferedAsync();
+        BufferedCommandResult res;
+        if (OperatingSystem.IsWindows())
+        {
+            res = await Cli.Wrap("powershell")
+                .WithArguments(
+                    $"-Command \"(Get-Item '{ChromeForTestingInstance.ChromePath}').VersionInfo.FileVersion\"")
+                .WithValidation(CommandResultValidation.None)
+                .ExecuteBufferedAsync();
+        }
+        else
+        {
+            res = await Cli.Wrap(ChromeForTestingInstance.ChromePath)
+                .WithArguments("--version")
+                .WithValidation(CommandResultValidation.None)
+                .ExecuteBufferedAsync();
+        }
 
         Assert.That(res.ExitCode, Is.EqualTo(0));
         Assert.That(res.StandardOutput, Is.Not.Empty);
